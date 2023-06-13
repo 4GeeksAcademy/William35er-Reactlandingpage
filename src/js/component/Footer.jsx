1472a4874cf25cbca585c4abfc4aba1d001b9cc4
William35er <125413709+William35er@users.noreply.github.com>
2023-06-13 23:15:37 +0000

    reactlandingpagechanges

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+//create your first component
+export const footer = () => {
+	return (
+		<div className="text-center">
+			
+		</div>
+	);
+};
+
---
+++ src/js/component/card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+//create your first component
+export const card = () => {
+	return (
+		<div className="text-center">
+			
+		</div>
+	);
+};
+
